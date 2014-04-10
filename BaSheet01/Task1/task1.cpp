c878cd16f8856714d25ce635741e4fd2383de907
BlueCrescent <BlueCrescent@gmx.de>
2014-04-10 15:39:07 +0200

    Added source file for task 1.

---
+++ BaSheet01/Task1/task1.cpp
@@ -0,0 +1,25 @@
+/*
+ * task1.cpp
+ *
+ *  Created on: 10.04.2014
+ *      Author: timm
+ */
+
+#include <cstdlib>
+#include <cstdio>
+#include <gsl/gsl_rng.h>
+
+int main() {
+  printf("%lf\n", (double)rand() / RAND_MAX);   // *
+
+  gsl_rng* r;
+  r = gsl_rng_alloc(gsl_rng_mt19937);
+  printf("%lf\n", gsl_rng_uniform(r));
+  gsl_rng_free(r);
+
+  return 0;
+}
+
+
+
+
