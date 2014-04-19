318a67c31bfd10b537c1524dc788addc7349c857
BlueCrescent <BlueCrescent@gmx.de>
2014-04-19 22:50:41 +0200

    Finished task 8: - Fix of online estimator computation. - Implemented test for identity of the two estimator implementations.

--- BaSheet01/Task8/estimator.cpp
+++ BaSheet01/Task8/estimator.cpp
@@ -14,26 +14,25 @@ inline double estimate_mean(const double* values, const unsigned int size) {
   return mean_estimate / size;
 }
 
-inline double estimate_variance(const double* values, const unsigned int size) {
-  double mean_estimate = estimate_mean(values, size);
-
+inline double estimate_variance(const double* values, const unsigned int size, const double mean) {
   double variance_estimate = 0;
   std::for_each(values, values + size,
-                [&](double val) {variance_estimate += (val - mean_estimate) * (val - mean_estimate);});
+                [&](double val) {variance_estimate += (val - mean) * (val - mean);});
   return variance_estimate / (size - 1);
 }
 
 double naive_variance_estimator(const double * values, const unsigned int size) {
-  return estimate_variance(values, size);
+  const double mean_estimate = estimate_mean(values, size);
+  return estimate_variance(values, size, mean_estimate);
 }
 
 double variance_estimator(const double * values, const unsigned int size) {
   double alpha = values[0];
   double beta = 0;
   for (unsigned int i = 1; i < size; ++i) {
-    double gamma = values[i] - alpha;
+    const double gamma = values[i] - alpha;
     alpha = alpha + gamma / (i + 1);
-    beta = beta + gamma / (i + 1);
+    beta = beta + gamma * gamma * i / (i + 1);
   }
   return beta / (size - 1);
 }
--- BaSheet01/Task8/estimator_test.cpp
+++ BaSheet01/Task8/estimator_test.cpp
@@ -8,8 +8,25 @@
 
 #include "gtest/gtest.h"
 
+#include <random>
+
 #include "estimator.h"
 
-TEST(EstimatorTest, TestThatNaiveEstimatorAndOnlineEstimatorAreEqual) {
+inline void fill_with_gauss_values(double * values, const unsigned int size, const double mean, const double variance) {
+  std::default_random_engine generator;
+  std::normal_distribution<double> distribution(mean, variance);
+  for (unsigned int i = 0; i < size; ++i) {
+    values[i] = distribution(generator);
+  }
+}
 
+TEST(EstimatorTest, TestThatNaiveEstimatorAndOnlineEstimatorAreEqual) {
+  const unsigned int size = 1000;
+  double values[size];
+  fill_with_gauss_values(values, size, 5., 2.);
+
+  const double naive_var_estimator = naive_variance_estimator(values, size);
+  const double online_var_estimator = variance_estimator(values, size);
+
+  ASSERT_DOUBLE_EQ(naive_var_estimator, online_var_estimator);
 }
