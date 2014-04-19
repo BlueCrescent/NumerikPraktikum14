37f31aab7fd6078d42ef86a1190619c7420f3c12
BlueCrescent <BlueCrescent@gmx.de>
2014-04-20 00:12:36 +0200

    Added C++11 implementation of the standard normal distribution to task 2 files.

---
+++ BaSheet01/Task2/Cpp11NormalDice.cpp
@@ -0,0 +1,22 @@
+/*
+ * Cpp11NormalDice.cpp
+ *
+ *  Created on: 20.04.2014
+ *      Author: BlueCrescent
+ */
+
+#include "Cpp11NormalDice.h"
+
+#include <ctime>
+
+Cpp11NormalDice::Cpp11NormalDice() {
+  this->generator.seed(time_t(NULL));
+}
+
+Cpp11NormalDice::~Cpp11NormalDice() {
+}
+
+double Cpp11UniformDice::roll() {
+  std::normal_distribution<double> distribution();
+  return distribution(generator);
+}
