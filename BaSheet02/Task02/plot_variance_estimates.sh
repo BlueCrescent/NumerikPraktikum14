bcdf7ddd8d558770a934b980d916b06fb5e8ff74
BlueCrescent <BlueCrescent@gmx.de>
2014-04-25 22:44:51 +0200

    Changed plot colors in sheet 2 task 2.

--- BaSheet02/Task02/plot_variance_estimates.sh
+++ BaSheet02/Task02/plot_variance_estimates.sh
@@ -8,7 +8,7 @@ set ylabel "mean / variance estimation"
 
 
 plot "data_sh2_2" using 1:3 pt 7 ps 2 title "variance estimation", \
-     "data_sh2_2" using 1:2 pt 7 ps 1 title "mean estimation"
+     "data_sh2_2" using 1:2 lt rgb "#0000FF" pt 7 ps 1 title "mean estimation"
 
 
 pause mouse
@@ -17,12 +17,12 @@ set term gif
 set output "sh2_task2_variance_estimates_plot.gif"
 
 plot "data_sh2_2" using 1:3 pt 7 ps 2 title "variance estimation", \
-     "data_sh2_2" using 1:2 pt 7 ps 1 title "mean estimation"
+     "data_sh2_2" using 1:2 lt rgb "#0000FF" pt 7 ps 1 title "mean estimation"
 
 set term postscript eps color blacktext "Helvetica" 14
 set output 'sh2_task2_variance_estimates_plot.eps'
 
 plot "data_sh2_2" using 1:3 pt 7 ps 2 title "variance estimation", \
-     "data_sh2_2" using 1:2 pt 7 ps 1 title "mean estimation"
+     "data_sh2_2" using 1:2 lt rgb "#0000FF" pt 7 ps 1 title "mean estimation"
 
 EOT
