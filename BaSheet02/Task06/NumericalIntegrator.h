ef43076634ed0ce28c6bd3916e83e85611945344
BlueCrescent <BlueCrescent@gmx.de>
2014-05-01 20:05:39 +0200

    Moved template function to header.

--- BaSheet02/Task06/NumericalIntegrator.h
+++ BaSheet02/Task06/NumericalIntegrator.h
@@ -9,20 +9,19 @@
 #define NUMERICALINTEGRATOR_H_
 
 #include <vector>
+
 #include <cassert>
+#include <cmath>
 
 class NumericalIntegrator {
 public:
-  class NodesAndWeights {
-    public:
+  struct NodesAndWeights {
+
       std::vector<double> Weights;
       std::vector<double> Nodes;
 
-      inline int getSize() const{
-        assert(Weights.size() == Nodes.size());
-        return Weights.size();
-      }
-    };
+      inline int getSize() const;
+  };
 
   template<typename T>
   double integrate(int level, T function) const;
@@ -31,8 +30,24 @@ public:
 
   virtual ~NumericalIntegrator(){};
 
-
 };
+
+inline int NumericalIntegrator::NodesAndWeights::getSize() const {
+  assert(Weights.size() == Nodes.size());
+  return Weights.size();
+}
+
+template<typename T>
+double NumericalIntegrator::integrate(int level, T function) const{
+  const int amount = pow(2,level) - 1;
+  const NodesAndWeights Parameters = getNodesAndWeights(amount);
+
+  double result = 0;
+  for(int i = 0; i < Parameters.getSize(); ++i){
+    result += function(Parameters.Nodes[i]) * Parameters.Weights[i];
+  }
+  return result;
+}
 
 
 
