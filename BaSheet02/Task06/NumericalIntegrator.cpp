c96838ad2f717e15dca465293e1cfbafdd5303be
boris <boris@boris-VirtualBox>
2014-05-01 16:00:07 +0200

    created abstract NumericalIntegrator Team

---
+++ BaSheet02/Task06/NumericalIntegrator.cpp
@@ -0,0 +1,22 @@
+/*
+ * NumericalIntegrator.cpp
+ *
+ *  Created on: 01.05.2014
+ *      Author: boris
+ */
+
+#include "NumericalIntegrator.h"
+#include <cmath>
+
+template<typename T>
+double NumericalIntegrator::integrate(int level, T function){
+  const int amount = pow(2,level) - 1;
+  const NodesAndWeights Parameters = getNodesAndWeights(amount);
+
+  double result = 0;
+  for(int i = 0; i < Parameters.getSize(); ++i){
+    result += function(Parameters.Nodes[i]) * Parameters.Weights[i];
+  }
+  return result;
+}
+
---
+++ BaSheet02/Task06/NumericalIntegrator.h
@@ -0,0 +1,39 @@
+/*
+ * NumericalIntegrator.h
+ *
+ *  Created on: 01.05.2014
+ *      Author: boris
+ */
+
+#ifndef NUMERICALINTEGRATOR_H_
+#define NUMERICALINTEGRATOR_H_
+
+#include <vector>
+#include <cassert>
+
+class NumericalIntegrator {
+public:
+  class NodesAndWeights {
+    public:
+      std::vector<double> Weights;
+      std::vector<double> Nodes;
+
+      inline int getSize() const{
+        assert(Weights.size() == Nodes.size());
+        return Weights.size();
+      }
+    };
+
+  template<typename T>
+  double integrate(int level, T function);
+
+  virtual NodesAndWeights getNodesAndWeights(int amount) = 0;
+
+  virtual ~NumericalIntegrator(){};
+
+
+};
+
+
+
+#endif /* NUMERICALINTEGRATOR_H_ */
