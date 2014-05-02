a4a828c4df874072ae937b844b4b551a9d7a3356
BlueCrescent <BlueCrescent@gmx.de>
2014-05-02 21:33:38 +0200

    Refactoring of task 10 code and minor fix of call ingrand function.

--- BaSheet02/Task10/main_s2_10.cpp
+++ BaSheet02/Task10/main_s2_10.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+
 #include "NumericalIntegrator.h"
 #include "TrapezoidalIntegrator.h"
 #include "ClenshawIntegrator.h"
@@ -17,65 +18,44 @@
 
 using namespace std;
 
-double callFunction(double x){
+inline void writeIntegrationDataToFile(const NumericalIntegrator & Integrator, int maxLevel, const char * filename);
+
+
+void main_s2_10(){
+  const int maxLevel = 11;
+
+  writeIntegrationDataToFile(TrapezoidalIntegrator(),   maxLevel, "data_callF_Trapezodial");
+  writeIntegrationDataToFile(ClenshawIntegrator(),      maxLevel, "data_callF_Clenshaw");
+  writeIntegrationDataToFile(GaussLegendreIntegrator(), maxLevel, "data_callF_GaussLegendre");
+  writeIntegrationDataToFile(MonteCarloIntegrator(),    maxLevel, "data_callF_MonteCarlo");
+}
+
+inline double callFunction(double x){
   const double S0 = 10., mu = 0.1, sigma = 0.2, T = 2., K = 10.;
-  const double value = exp(S0 * (mu - 0.5 * sigma * sigma) * T + sigma * sqrt(T) * x);
+  const double value = S0 * exp((mu - 0.5 * sigma * sigma) * T + sigma * sqrt(T) * x);
   return value > K ? value - K : 0.;
 }
 
-void writeDataToFileTrapezodial(int level){
-  TrapezoidalIntegrator Integrator0;
-  ofstream myfile;
-  cout<<"open data_callF_Trapezodial... ";
-  myfile.open ("data_callF_Trapezodial");
-  myfile<<std::setprecision(20);
-  cout<<"write data..."<<endl;
-  for(int i = 1; i < level; ++i){
-    myfile<< i << " " << Integrator0.integrate(i, &callFunction) << "\n";
-  }
-  myfile.close();
-}
-void writeDataToFileClenshaw(int level){
-  ClenshawIntegrator Integrator1;
-  ofstream myfile;
-  cout<<"open data_callF_Clenshaw... ";
-  myfile.open ("data_callF_Clenshaw");
-  myfile<<std::setprecision(20);
-  cout<<"write data... "<<endl;
-  for(int i = 1; i < level; ++i){
-    myfile<< i << " " << Integrator1.integrate(i, &callFunction) << "\n";
-  }
-  myfile.close();
-}
-void writeDataToFileGaussLegendre(int level){
-  GaussLegendreIntegrator Integrator2;
-  ofstream myfile;
-  cout<<"open data_callF_GaussLegendre... ";
-  myfile.open ("data_callF_GaussLegendre");
-  myfile<<std::setprecision(20);
-  cout<<"write data... "<<endl;
-  for(int i = 1; i < level; ++i){
-    myfile<< i << " " << Integrator2.integrate(i, &callFunction) << "\n";
-  }
-  myfile.close();
-}
-void writeDataToFileMonteCarlo(int level){
-  MonteCarloIntegrator Integrator3;
-  ofstream myfile;
-  cout<<"open data_callF_MonteCarlo... ";
-  myfile.open ("data_callF_MonteCarlo");
-  myfile<<std::setprecision(20);
-  cout<<"write data..."<<endl;
-  for(int i = 1; i < level; ++i){
-    myfile<< i << " " << Integrator3.integrate(i, &callFunction) << "\n";
-  }
-  myfile.close();
+inline void openOutputFile(ofstream& myfile, const char * filename) {
+  cout << "    opening " << filename << "... ";
+  myfile.open(filename);
+  myfile << std::setprecision(20);
+  cout << "    writing data..." << endl;
 }
 
-void main_s2_10(){
-  int level = 12;
-  writeDataToFileTrapezodial(level);
-  writeDataToFileClenshaw(level);
-  writeDataToFileGaussLegendre(level);
-  writeDataToFileMonteCarlo(level);
+inline void generateOutputDataWithCallFunc(int maxLevel, ostream & myfile, const NumericalIntegrator & Integrator) {
+  for (int l = 1; l <= maxLevel; ++l) {
+    myfile << l << " " << Integrator.integrate(l, callFunction) << "\n";
+  }
 }
+
+inline void writeIntegrationDataToFile(const NumericalIntegrator & Integrator, int maxLevel, const char * filename) {
+  ofstream outputFile;
+  openOutputFile(outputFile, filename);
+
+  generateOutputDataWithCallFunc(maxLevel, outputFile, Integrator);
+
+  outputFile.close();
+}
+
+
