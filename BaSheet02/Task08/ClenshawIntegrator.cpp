602ad7afa64bc967ebf861d3fd67625a58a9b083
BlueCrescent <BlueCrescent@gmx.de>
2014-05-03 12:55:19 +0200

    Small fix for Clenshaw Curtis.

--- BaSheet02/Task08/ClenshawIntegrator.cpp
+++ BaSheet02/Task08/ClenshawIntegrator.cpp
@@ -10,25 +10,25 @@
 
 inline void ClenshawIntegrator::generateNodes(const int amount, NodesAndWeights& params) const {
   const double factor = M_PI / (amount + 1.);
-  for (int i = 0; i < amount; ++i) {
+  for (int i = 1; i <= amount; ++i) {
       const double node = 0.5 * (1. - cos(i * factor));
       params.Nodes.push_back(node);
   }
 }
 
 inline double weightInnerSum(const int sumUpper, const double piFactor) {
-  double weightAdd = 0.;
+  double weightSum = 0.;
   for (int j = 1; j <= sumUpper; ++j) {
-    weightAdd += 1. / (2. * j - 1.) * sin((2. * j - 1.) * piFactor);
+    weightSum += 1. / (2. * j - 1.) * sin((2. * j - 1.) * piFactor);
   }
-  return weightAdd;
+  return weightSum;
 }
 
 inline void ClenshawIntegrator::generateWeights(const int amount, NodesAndWeights& params) const {
   const double factor = 1. / (amount + 1.);
-  for (int i = 0; i < amount; ++i) {
+  for (int i = 1; i <= amount; ++i) {
     const double piFactor = M_PI * i * factor;
-    const double weight = 2. * factor * sin(piFactor) + weightInnerSum((amount + 1) / 2, piFactor);
+    const double weight = 2. * factor * sin(piFactor) * weightInnerSum((amount + 1) / 2, piFactor);
     params.Weights.push_back(weight);
   }
 }
