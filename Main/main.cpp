c13700851b4a7c971121285a956c4352aa8a4428
BlueCrescent <BlueCrescent@gmx.de>
2014-05-04 23:24:05 +0200

    Reactivated stuff in main. Fixed gif.

--- Main/main.cpp
+++ Main/main.cpp
@@ -11,12 +11,12 @@ int main(){
 //  main9();
 //  std::cout << "Starting sheet 1 task 10..." << std::endl;
 //  main10();
-//  std::cout << "Starting sheet 2 task 1..." << std::endl;
-//  main_s2_01();
-//  std::cout << "Starting sheet 2 task 2..." << std::endl;
-//  main_s2_02();
-//  std::cout << "Starting sheet 2 task 4..." << std::endl;
-//  main_s2_04();
+  std::cout << "Starting sheet 2 task 1..." << std::endl;
+  main_s2_01();
+  std::cout << "Starting sheet 2 task 2..." << std::endl;
+  main_s2_02();
+  std::cout << "Starting sheet 2 task 4..." << std::endl;
+  main_s2_04();
   std::cout << "Starting sheet 2 task 9..." << std::endl;
   main_s2_09();
   std::cout << "Starting sheet 2 task 10..." << std::endl;
