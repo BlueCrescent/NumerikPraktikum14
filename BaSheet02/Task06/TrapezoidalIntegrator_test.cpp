2b8ba2b055ced3366987b6a9a949e3ae2c1c4b9e
BlueCrescent <BlueCrescent@gmx.de>
2014-05-07 21:29:54 +0200

    Added some tests to trapezoidal integrator.

--- BaSheet02/Task06/TrapezoidalIntegrator_test.cpp
+++ BaSheet02/Task06/TrapezoidalIntegrator_test.cpp
@@ -5,5 +5,40 @@
  *      Author: boris
  */
 
-#include <Task06/TrapezoidalIntegrator.h>
+#include "TrapezoidalIntegrator.h"
 
+#include "gtest/gtest.h"
+
+class TrapezoidalIntegratorTest : public ::testing::Test {
+public:
+
+  TrapezoidalIntegrator integrator;
+};
+
+TEST_F(TrapezoidalIntegratorTest, PolynomialIntegrationExactnessTestsDegree1) {
+  ASSERT_DOUBLE_EQ(0.5, integrator.integrate(2, [](double x){return x;}));
+}
+
+double piecewise_linear(double x) {
+//  x += 1. / 8.;
+//  if (x <= 0.)
+//    return 0.;
+  if (x <= 1. / 8.)
+    return 8 * x;
+  if (x <= 2. / 8.)
+    return 1.;
+  if (x <= 3. / 8.)
+    return 8. * x - 1.;
+  if (x <= 4. / 8.)
+    return - 8. * x + 5.;
+  if (x <= 5. / 8.)
+    return 1.;
+  if (x <= 6. / 8.)
+    return - 8. * x + 6.;
+  else
+    return 0.;
+}
+
+TEST_F(TrapezoidalIntegratorTest, PiecewiseLinearTest) {
+  ASSERT_DOUBLE_EQ(3. / 4., integrator.integrate(3, piecewise_linear));
+}
