d55b74cb7118e49ae9e7a0509de1df4226d29011
BlueCrescent <BlueCrescent@gmx.de>
2014-05-17 00:37:36 +0200

    Fixed task 9 pdf plots.

--- BaSheet03/Task09/sh3_09_point_plot.sh
+++ BaSheet03/Task09/sh3_09_point_plot.sh
@@ -7,41 +7,41 @@ set xlabel "x"
 set ylabel "y"
 
 set title "Nodes of 2D trapezoidal product rules"
-plot "data_sh3_09_trapezoidal" using 1:2 with p lt rgb "red"
+plot "data_sh3_09_trapezoidal" using 1:2 with p lt rgb "red" notitle
 pause mouse
 
 set title "Nodes of 2D Gauss-Legendre product rules"
-plot "data_sh3_09_gaussLegendre" using 1:2 with p lt rgb "blue"
+plot "data_sh3_09_gaussLegendre" using 1:2 with p lt rgb "blue" notitle
 pause mouse
 
 set title "Nodes of 2D Clenshaw-Curtis product rules"
-plot "data_sh3_09_clenshawCurtis" using 1:2 with p lt rgb "green"
+plot "data_sh3_09_clenshawCurtis" using 1:2 with p lt rgb "green" notitle
 pause mouse
 
 set term gif
 
 set output "sh3_task9_point_plot_trapezoidal.gif"
 set title "Nodes of 2D trapezoidal product rules"
-plot "data_sh3_09_trapezoidal" using 1:2 with p lt rgb "red"
+plot "data_sh3_09_trapezoidal" using 1:2 with p lt rgb "red" notitle
 set output "sh3_task9_point_plot_gaussLegendre.gif"
 set title "Nodes of 2D Gauss-Legendre product rules"
-plot "data_sh3_09_gaussLegendre" using 1:2 with p lt rgb "blue"
+plot "data_sh3_09_gaussLegendre" using 1:2 with p lt rgb "blue" notitle
 set output "sh3_task9_point_plot_clenshawCurtis.gif"
 set title "Nodes of 2D Clenshaw-Curtis product rules"
-plot "data_sh3_09_clenshawCurtis" using 1:2 with p lt rgb "green"
+plot "data_sh3_09_clenshawCurtis" using 1:2 with p lt rgb "green" notitle
 
-set term pdf color blacktext "Helvetica" 14
+set term pdf
 
 set output 'sh3_task9_point_plot_trapezoidal.pdf'
 set title "Nodes of 2D trapezoidal product rules"
-plot "data_sh3_09_trapezoidal" using 1:2 with p lt rgb "red"
+plot "data_sh3_09_trapezoidal" using 1:2 with p lt rgb "red" notitle
 set output 'sh3_task9_point_plot_gaussLegendre.pdf'
 set title "Nodes of 2D Gauss-Legendre product rules"
-plot "data_sh3_09_gaussLegendre" using 1:2 with p lt rgb "blue"
+plot "data_sh3_09_gaussLegendre" using 1:2 with p lt rgb "blue" notitle
 set output 'sh3_task9_point_plot_clenshawCurtis.pdf'
 set title "Nodes of 2D Clenshaw-Curtis product rules"
-plot "data_sh3_09_clenshawCurtis" using 1:2 with p lt rgb "green"
+plot "data_sh3_09_clenshawCurtis" using 1:2 with p lt rgb "green" notitle
 
 EOT
-
+#set term pdf color blacktext "Helvetica" 14
 #set term postscript eps color blacktext "Helvetica" 14
