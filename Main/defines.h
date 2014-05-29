3ef2f6a405634ee26e95dd7218708bbb7e2da69d
BlueCrescent <BlueCrescent@gmx.de>
2014-05-29 10:51:18 +0200

    Modified main(): Added defines to regulate which sheets should be executed.

---
+++ Main/defines.h
@@ -0,0 +1,18 @@
+/*
+ * defines.h
+ *
+ *  Created on: 29.05.2014
+ *      Author: BlueCrescent
+ */
+
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
+
+//#define SHEET1
+//#define SHEET2
+#define SHEET3
+#define SHEET4
+#define SHEET5
+
+#endif /* DEFINES_H_ */
--- Main/main.cpp
+++ Main/main.cpp
@@ -1,40 +1,51 @@
 #include "mainIncludes.h"
+#include "defines.h"
 
 #include <iostream>
 
 int main(){
-//  std::cout << "Starting sheet 1 task 3..." << std::endl;
-//  main3();
-//  std::cout << "Starting sheet 1 task 6..." << std::endl;
-//  main6();
-//  std::cout << "Starting sheet 1 task 9..." << std::endl;
-//  main9();
-//  std::cout << "Starting sheet 1 task 10..." << std::endl;
-//  main10();
-//  std::cout << "Starting sheet 2 task 1..." << std::endl;
-//  main_s2_01();
-//  std::cout << "Starting sheet 2 task 2..." << std::endl;
-//  main_s2_02();
-//  std::cout << "Starting sheet 2 task 4..." << std::endl;
-//  main_s2_04();
-//  std::cout << "Starting sheet 2 task 9..." << std::endl;
-//  main_s2_09();
-//  std::cout << "Starting sheet 2 task 10..." << std::endl;
-//  main_s2_10();
-//  std::cout << "Starting sheet 3 task 3..." << std::endl;
-//  main_s3_03();
-//  std::cout << "Starting sheet 3 task 4..." << std::endl;
-//  main_s3_04();
-//  std::cout << "Starting sheet 3 task 5..." << std::endl;
-//  main_s3_05();
-//  std::cout << "Starting sheet 3 task 7..." << std::endl;
-//  main_s3_07();
-//  std::cout << "Starting sheet 3 task 9..." << std::endl;
-//  main_s3_09();
+#ifdef SHEET1
+  std::cout << "Starting sheet 1 task 3..." << std::endl;
+  main3();
+  std::cout << "Starting sheet 1 task 6..." << std::endl;
+  main6();
+  std::cout << "Starting sheet 1 task 9..." << std::endl;
+  main9();
+  std::cout << "Starting sheet 1 task 10..." << std::endl;
+  main10();
+#endif
+#ifdef SHEET2
+  std::cout << "Starting sheet 2 task 1..." << std::endl;
+  main_s2_01();
+  std::cout << "Starting sheet 2 task 2..." << std::endl;
+  main_s2_02();
+  std::cout << "Starting sheet 2 task 4..." << std::endl;
+  main_s2_04();
+  std::cout << "Starting sheet 2 task 9..." << std::endl;
+  main_s2_09();
+  std::cout << "Starting sheet 2 task 10..." << std::endl;
+  main_s2_10();
+  std::cout << "Starting sheet 3 task 3..." << std::endl;
+#endif
+#ifdef SHEET3
+  main_s3_03();
+  std::cout << "Starting sheet 3 task 4..." << std::endl;
+  main_s3_04();
+  std::cout << "Starting sheet 3 task 5..." << std::endl;
+  main_s3_05();
+  std::cout << "Starting sheet 3 task 7..." << std::endl;
+  main_s3_07();
+  std::cout << "Starting sheet 3 task 9..." << std::endl;
+  main_s3_09();
   std::cout << "Starting sheet 3 task 11..." << std::endl;
   main_s3_11();
   std::cout << "Starting sheet 3 task 12..." << std::endl;
   main_s3_12();
+#endif
+#ifdef SHEET4
+#endif
+#ifdef SHEET5
+#endif
   std::cout << "Over and out." << std::endl << std::endl << std::endl;
   return 0;
 }
