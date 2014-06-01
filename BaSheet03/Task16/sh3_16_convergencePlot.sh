d5c900494e2094dcf5ff7e0f432f4ab02b91438b
BlueCrescent <BlueCrescent@gmx.de>
2014-06-01 22:16:43 +0200

    Fixed a typo.

--- BaSheet03/Task16/sh3_16_convergencePlot.sh
+++ BaSheet03/Task16/sh3_16_convergencePlot.sh
@@ -12,60 +12,60 @@ set title "convergence plot for d = 1"
 plot "data_s3_16_d=1" using 1:2 with l title "RandWalk - Monte Carlo", \
  "data_s3_16_d=1" using 1:3 with l title "RandWalk - Quasi Monte Carlo", \
  "data_s3_16_d=1" using 1:4 with l title "RandWalk - Product - Trapez", \
- "data_s3_16_d=1" using 1:5 with l title "RandWalk - Product - Clensah Curtis", \
+ "data_s3_16_d=1" using 1:5 with l title "RandWalk - Product - Clensha Curtis", \
  "data_s3_16_d=1" using 1:6 with l title "RandWalk - Sparse Grid - Trapez", \
- "data_s3_16_d=1" using 1:7 with l title "RandWalk - Sparse Grid - Clensah Curtis", \
+ "data_s3_16_d=1" using 1:7 with l title "RandWalk - Sparse Grid - Clensha Curtis", \
  "data_s3_16_d=1" using 1:8 with l title "BrownBridge - Monte Carlo", \
  "data_s3_16_d=1" using 1:9 with l title "BrownBridge - Quasi Monte Carlo", \
  "data_s3_16_d=1" using 1:10 with l title "BrownBridge - Product - Trapez", \
- "data_s3_16_d=1" using 1:11 with l title "BrownBridge - Product - Clensah Curtis", \
+ "data_s3_16_d=1" using 1:11 with l title "BrownBridge - Product - Clensha Curtis", \
  "data_s3_16_d=1" using 1:12 with l title "BrownBridge - Sparse Grid - Trapez", \
- "data_s3_16_d=1" using 1:13 with l title "BrownBridge - Sparse Grid - Clensah Curtis"
+ "data_s3_16_d=1" using 1:13 with l title "BrownBridge - Sparse Grid - Clensha Curtis"
 pause mouse
 
 set title "convergence plot for d = 2"
 plot "data_s3_16_d=2" using 1:2 with l title "RandWalk - Monte Carlo", \
  "data_s3_16_d=2" using 1:3 with l title "RandWalk - Quasi Monte Carlo", \
  "data_s3_16_d=2" using 1:4 with l title "RandWalk - Product - Trapez", \
- "data_s3_16_d=2" using 1:5 with l title "RandWalk - Product - Clensah Curtis", \
+ "data_s3_16_d=2" using 1:5 with l title "RandWalk - Product - Clensha Curtis", \
  "data_s3_16_d=2" using 1:6 with l title "RandWalk - Sparse Grid - Trapez", \
- "data_s3_16_d=2" using 1:7 with l title "RandWalk - Sparse Grid - Clensah Curtis", \
+ "data_s3_16_d=2" using 1:7 with l title "RandWalk - Sparse Grid - Clensha Curtis", \
  "data_s3_16_d=2" using 1:8 with l title "BrownBridge - Monte Carlo", \
  "data_s3_16_d=2" using 1:9 with l title "BrownBridge - Quasi Monte Carlo", \
  "data_s3_16_d=2" using 1:10 with l title "BrownBridge - Product - Trapez", \
- "data_s3_16_d=2" using 1:11 with l title "BrownBridge - Product - Clensah Curtis", \
+ "data_s3_16_d=2" using 1:11 with l title "BrownBridge - Product - Clensha Curtis", \
  "data_s3_16_d=2" using 1:12 with l title "BrownBridge - Sparse Grid - Trapez", \
- "data_s3_16_d=2" using 1:13 with l title "BrownBridge - Sparse Grid - Clensah Curtis"
+ "data_s3_16_d=2" using 1:13 with l title "BrownBridge - Sparse Grid - Clensha Curtis"
 pause mouse
 
 set title "convergence plot for d = 4"
 plot "data_s3_16_d=4" using 1:2 with l title "RandWalk - Monte Carlo", \
  "data_s3_16_d=4" using 1:3 with l title "RandWalk - Quasi Monte Carlo", \
  "data_s3_16_d=4" using 1:4 with l title "RandWalk - Product - Trapez", \
- "data_s3_16_d=4" using 1:5 with l title "RandWalk - Product - Clensah Curtis", \
+ "data_s3_16_d=4" using 1:5 with l title "RandWalk - Product - Clensha Curtis", \
  "data_s3_16_d=4" using 1:6 with l title "RandWalk - Sparse Grid - Trapez", \
- "data_s3_16_d=4" using 1:7 with l title "RandWalk - Sparse Grid - Clensah Curtis", \
+ "data_s3_16_d=4" using 1:7 with l title "RandWalk - Sparse Grid - Clensha Curtis", \
  "data_s3_16_d=4" using 1:8 with l title "BrownBridge - Monte Carlo", \
  "data_s3_16_d=4" using 1:9 with l title "BrownBridge - Quasi Monte Carlo", \
  "data_s3_16_d=4" using 1:10 with l title "BrownBridge - Product - Trapez", \
- "data_s3_16_d=4" using 1:11 with l title "BrownBridge - Product - Clensah Curtis", \
+ "data_s3_16_d=4" using 1:11 with l title "BrownBridge - Product - Clensha Curtis", \
  "data_s3_16_d=4" using 1:12 with l title "BrownBridge - Sparse Grid - Trapez", \
- "data_s3_16_d=4" using 1:13 with l title "BrownBridge - Sparse Grid - Clensah Curtis"
+ "data_s3_16_d=4" using 1:13 with l title "BrownBridge - Sparse Grid - Clensha Curtis"
 pause mouse
 
 set title "convergence plot for d = 8"
 plot "data_s3_16_d=8" using 1:2 with l title "RandWalk - Monte Carlo", \
  "data_s3_16_d=8" using 1:3 with l title "RandWalk - Quasi Monte Carlo", \
  "data_s3_16_d=8" using 1:4 with l title "RandWalk - Product - Trapez", \
- "data_s3_16_d=8" using 1:5 with l title "RandWalk - Product - Clensah Curtis", \
+ "data_s3_16_d=8" using 1:5 with l title "RandWalk - Product - Clensha Curtis", \
  "data_s3_16_d=8" using 1:6 with l title "RandWalk - Sparse Grid - Trapez", \
- "data_s3_16_d=8" using 1:7 with l title "RandWalk - Sparse Grid - Clensah Curtis", \
+ "data_s3_16_d=8" using 1:7 with l title "RandWalk - Sparse Grid - Clensha Curtis", \
  "data_s3_16_d=8" using 1:8 with l title "BrownBridge - Monte Carlo", \
  "data_s3_16_d=8" using 1:9 with l title "BrownBridge - Quasi Monte Carlo", \
  "data_s3_16_d=8" using 1:10 with l title "BrownBridge - Product - Trapez", \
- "data_s3_16_d=8" using 1:11 with l title "BrownBridge - Product - Clensah Curtis", \
+ "data_s3_16_d=8" using 1:11 with l title "BrownBridge - Product - Clensha Curtis", \
  "data_s3_16_d=8" using 1:12 with l title "BrownBridge - Sparse Grid - Trapez", \
- "data_s3_16_d=8" using 1:13 with l title "BrownBridge - Sparse Grid - Clensah Curtis"
+ "data_s3_16_d=8" using 1:13 with l title "BrownBridge - Sparse Grid - Clensha Curtis"
 pause mouse
 
 
@@ -76,60 +76,60 @@ set title "convergence plot for d = 1"
 plot "data_s3_16_d=1" using 1:2 with l title "RandWalk - Monte Carlo", \
  "data_s3_16_d=1" using 1:3 with l title "RandWalk - Quasi Monte Carlo", \
  "data_s3_16_d=1" using 1:4 with l title "RandWalk - Product - Trapez", \
- "data_s3_16_d=1" using 1:5 with l title "RandWalk - Product - Clensah Curtis", \
+ "data_s3_16_d=1" using 1:5 with l title "RandWalk - Product - Clensha Curtis", \
  "data_s3_16_d=1" using 1:6 with l title "RandWalk - Sparse Grid - Trapez", \
- "data_s3_16_d=1" using 1:7 with l title "RandWalk - Sparse Grid - Clensah Curtis", \
+ "data_s3_16_d=1" using 1:7 with l title "RandWalk - Sparse Grid - Clensha Curtis", \
  "data_s3_16_d=1" using 1:8 with l title "BrownBridge - Monte Carlo", \
  "data_s3_16_d=1" using 1:9 with l title "BrownBridge - Quasi Monte Carlo", \
  "data_s3_16_d=1" using 1:10 with l title "BrownBridge - Product - Trapez", \
- "data_s3_16_d=1" using 1:11 with l title "BrownBridge - Product - Clensah Curtis", \
+ "data_s3_16_d=1" using 1:11 with l title "BrownBridge - Product - Clensha Curtis", \
  "data_s3_16_d=1" using 1:12 with l title "BrownBridge - Sparse Grid - Trapez", \
- "data_s3_16_d=1" using 1:13 with l title "BrownBridge - Sparse Grid - Clensah Curtis"
+ "data_s3_16_d=1" using 1:13 with l title "BrownBridge - Sparse Grid - Clensha Curtis"
 
 set output "sh3_task16_convergencePlotd2.pdf"
 set title "convergence plot for d = 2"
 plot "data_s3_16_d=2" using 1:2 with l title "RandWalk - Monte Carlo", \
  "data_s3_16_d=2" using 1:3 with l title "RandWalk - Quasi Monte Carlo", \
  "data_s3_16_d=2" using 1:4 with l title "RandWalk - Product - Trapez", \
- "data_s3_16_d=2" using 1:5 with l title "RandWalk - Product - Clensah Curtis", \
+ "data_s3_16_d=2" using 1:5 with l title "RandWalk - Product - Clensha Curtis", \
  "data_s3_16_d=2" using 1:6 with l title "RandWalk - Sparse Grid - Trapez", \
- "data_s3_16_d=2" using 1:7 with l title "RandWalk - Sparse Grid - Clensah Curtis", \
+ "data_s3_16_d=2" using 1:7 with l title "RandWalk - Sparse Grid - Clensha Curtis", \
  "data_s3_16_d=2" using 1:8 with l title "BrownBridge - Monte Carlo", \
  "data_s3_16_d=2" using 1:9 with l title "BrownBridge - Quasi Monte Carlo", \
  "data_s3_16_d=2" using 1:10 with l title "BrownBridge - Product - Trapez", \
- "data_s3_16_d=2" using 1:11 with l title "BrownBridge - Product - Clensah Curtis", \
+ "data_s3_16_d=2" using 1:11 with l title "BrownBridge - Product - Clensha Curtis", \
  "data_s3_16_d=2" using 1:12 with l title "BrownBridge - Sparse Grid - Trapez", \
- "data_s3_16_d=2" using 1:13 with l title "BrownBridge - Sparse Grid - Clensah Curtis"
+ "data_s3_16_d=2" using 1:13 with l title "BrownBridge - Sparse Grid - Clensha Curtis"
 
 set output "sh3_task16_convergencePlotd4.pdf"
 set title "convergence plot for d = 4"
 plot "data_s3_16_d=4" using 1:2 with l title "RandWalk - Monte Carlo", \
  "data_s3_16_d=4" using 1:3 with l title "RandWalk - Quasi Monte Carlo", \
  "data_s3_16_d=4" using 1:4 with l title "RandWalk - Product - Trapez", \
- "data_s3_16_d=4" using 1:5 with l title "RandWalk - Product - Clensah Curtis", \
+ "data_s3_16_d=4" using 1:5 with l title "RandWalk - Product - Clensha Curtis", \
  "data_s3_16_d=4" using 1:6 with l title "RandWalk - Sparse Grid - Trapez", \
- "data_s3_16_d=4" using 1:7 with l title "RandWalk - Sparse Grid - Clensah Curtis", \
+ "data_s3_16_d=4" using 1:7 with l title "RandWalk - Sparse Grid - Clensha Curtis", \
  "data_s3_16_d=4" using 1:8 with l title "BrownBridge - Monte Carlo", \
  "data_s3_16_d=4" using 1:9 with l title "BrownBridge - Quasi Monte Carlo", \
  "data_s3_16_d=4" using 1:10 with l title "BrownBridge - Product - Trapez", \
- "data_s3_16_d=4" using 1:11 with l title "BrownBridge - Product - Clensah Curtis", \
+ "data_s3_16_d=4" using 1:11 with l title "BrownBridge - Product - Clensha Curtis", \
  "data_s3_16_d=4" using 1:12 with l title "BrownBridge - Sparse Grid - Trapez", \
- "data_s3_16_d=4" using 1:13 with l title "BrownBridge - Sparse Grid - Clensah Curtis"
+ "data_s3_16_d=4" using 1:13 with l title "BrownBridge - Sparse Grid - Clensha Curtis"
 
 set output "sh3_task16_convergencePlotd8.pdf"
 set title "convergence plot for d = 8"
 plot "data_s3_16_d=8" using 1:2 with l title "RandWalk - Monte Carlo", \
  "data_s3_16_d=8" using 1:3 with l title "RandWalk - Quasi Monte Carlo", \
  "data_s3_16_d=8" using 1:4 with l title "RandWalk - Product - Trapez", \
- "data_s3_16_d=8" using 1:5 with l title "RandWalk - Product - Clensah Curtis", \
+ "data_s3_16_d=8" using 1:5 with l title "RandWalk - Product - Clensha Curtis", \
  "data_s3_16_d=8" using 1:6 with l title "RandWalk - Sparse Grid - Trapez", \
- "data_s3_16_d=8" using 1:7 with l title "RandWalk - Sparse Grid - Clensah Curtis", \
+ "data_s3_16_d=8" using 1:7 with l title "RandWalk - Sparse Grid - Clensha Curtis", \
  "data_s3_16_d=8" using 1:8 with l title "BrownBridge - Monte Carlo", \
  "data_s3_16_d=8" using 1:9 with l title "BrownBridge - Quasi Monte Carlo", \
  "data_s3_16_d=8" using 1:10 with l title "BrownBridge - Product - Trapez", \
- "data_s3_16_d=8" using 1:11 with l title "BrownBridge - Product - Clensah Curtis", \
+ "data_s3_16_d=8" using 1:11 with l title "BrownBridge - Product - Clensha Curtis", \
  "data_s3_16_d=8" using 1:12 with l title "BrownBridge - Sparse Grid - Trapez", \
- "data_s3_16_d=8" using 1:13 with l title "BrownBridge - Sparse Grid - Clensah Curtis"
+ "data_s3_16_d=8" using 1:13 with l title "BrownBridge - Sparse Grid - Clensha Curtis"
 pause mouse
 
 EOT
