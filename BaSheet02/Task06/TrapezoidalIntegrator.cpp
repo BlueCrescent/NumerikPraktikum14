78b616bdfbe2611d8557cb2b72966156ed803c31
BlueCrescent <BlueCrescent@gmx.de>
2014-06-01 21:04:19 +0200

    Fixed trapezoidal integrator case l = 1.

--- BaSheet02/Task06/TrapezoidalIntegrator.cpp
+++ BaSheet02/Task06/TrapezoidalIntegrator.cpp
@@ -8,19 +8,27 @@
 #include "TrapezoidalIntegrator.h"
 
 void TrapezoidalIntegrator::generateWeights(NodesAndWeights& returnParam, const double factor, int amount) const {
-  returnParam.Weights.push_back(3. / 2. * factor);
-  for (int i = 1; i < amount - 1; ++i) {
-    returnParam.Weights.push_back(factor);
+  if (amount == 1)
+    returnParam.Weights.push_back(1.);
+  else {
+    returnParam.Weights.push_back(3. / 2. * factor);
+    for (int i = 1; i < amount - 1; ++i) {
+      returnParam.Weights.push_back(factor);
+    }
+    returnParam.Weights.push_back(3. / 2. * factor);
   }
-  returnParam.Weights.push_back(3. / 2. * factor);
 }
 
 void TrapezoidalIntegrator::generateNodes(NodesAndWeights& returnParam, double factor, int amount) const {
-  returnParam.Nodes.push_back(factor);
-  for (int i = 2; i <= amount - 1; ++i) {
-    returnParam.Nodes.push_back(factor*i);
+  if (amount == 1)
+    returnParam.Nodes.push_back(0.5);
+  else {
+    returnParam.Nodes.push_back(factor);
+    for (int i = 2; i <= amount - 1; ++i) {
+      returnParam.Nodes.push_back(factor*i);
+    }
+    returnParam.Nodes.push_back(factor*amount);
   }
-  returnParam.Nodes.push_back(factor*amount);
 }
 
 TrapezoidalIntegrator::NodesAndWeights TrapezoidalIntegrator::returnZeroCaseStuff() const {
