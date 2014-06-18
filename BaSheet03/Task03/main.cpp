f198607467e680bab5e059ac4a43f79d2bf0fd78
BlueCrescent <BlueCrescent@gmx.de>
2014-06-19 00:17:45 +0200

    Removed debug code from sheet 3 task 3.

--- BaSheet03/Task03/main.cpp
+++ BaSheet03/Task03/main.cpp
@@ -24,37 +24,22 @@ void main_s3_03() {
   simulateGeometricAverage(M2, "data_s3_03_M200");
 }
 
-inline double exactExpectationForCallOptionValue2() {
-  const double S0 = 10., mu = 0.1, sigma = 0.25, T = 1., K = 10.;
-  if (K == 0.)
-    return S0 * exp(mu * T);
-  const double X = 1. / (sigma * sqrt(T)) * (log(K / S0) - (mu - sigma * sigma / 2) * T);
-  const double expectation = S0 * exp(mu * T) * NormalCDF(sigma * sqrt(T) - X) - K * NormalCDF(- X);
-  return expectation;
-}
-
 void simulateAndPrintRelativeError(int M, std::ostream& outStream) {
-  const double start_value = 10, r = 0.1, sigma = 0.25, strike = 10, T = 1., delta_t = T / M;
+  const double start_value = 10., r = 0.1, sigma = 0.25, strike = 10., T = 1., delta_t = T / M;
   const geometricBMParameter Parameters {start_value, r, sigma, delta_t};
   Cpp11NormalDice Dice;
 
   const double expectedPayoff1 = calc_discrete_geometric_fairP(start_value, r, sigma, strike, T, M);
   const double discountFactor = exp(- r * T);
 
-  double compVal = start_value*exp(r) - strike;
-
   double sum = 0;
-  for (int i = 1; i < 100000; i++) {
-//  int i = 1;
+  for (int i = 1; i <= 100000; i++) {
     geometricBM Path(Parameters, Dice);
     const std::vector<double> values = Path.compute_path_values(M);
     sum += evaluate_discr_geometric_payoff(values, strike);
-    if ((i + 1) % 1000 == 0) {
+    if (i % 1000 == 0)
       outStream << i << " " << fabs(discountFactor * sum / i - expectedPayoff1) / expectedPayoff1 << std::endl;
-      outStream << i << " " << discountFactor * sum / i << " " << " " << expectedPayoff1 <<" "<< std::endl;//<< discountFactor * exactExpectationForCallOptionValue2() << std::endl;
-    }
   }
-
 }
 
 void simulateGeometricAverage(int M, const char * filename) {
