3ebddba28eba2d109130ab9bbc0173e53dd9be4a
BlueCrescent <BlueCrescent@gmx.de>
2014-06-22 12:01:30 +0200

    Finished sheet 4 task 7 output generation.

--- BaSheet04/Task07/main_s4_07.cpp
+++ BaSheet04/Task07/main_s4_07.cpp
@@ -5,7 +5,8 @@
  *      Author: boris
  */
 
-#include <iostream>
+#include <fstream>
+#include <iomanip>
 
 #include "NewtonRaphson.h"
 
@@ -26,60 +27,60 @@ namespace {
 }
 
 template<bool isCall>
-void runTest(const double startSigma = computeDefaultSigma<isCall>(S0, r, T, K, historicalSigma, maxIterations)) {
-//  const double startSigma = 10; // computeDefaultSigma<isCall>(S0, r, T, K, historicalSigma, maxIterations);
+void runOneNewtonRaphsonTest(const double startSigma = computeDefaultSigma<isCall>(S0, r, T, K, historicalSigma, maxIterations)) {
 
-//  std::cout << "Running with sigma = " << startSigma << ". The default sigma would be "
-//            << computeDefaultSigma<isCall>(S0, r, T, K, historicalSigma, maxIterations) << "." << std::endl;
-
-  std::cout << "Running with historical sigma = " << historicalSigma
-            << " and start sigma = " <<  startSigma << "." << std::endl;
+//  std::cout << "Running with historical sigma = " << historicalSigma
+//            << " and start sigma = " <<  startSigma << "." << std::endl;
   try {
     const double impliedVolatility = executeNewtonRaphsonAlgo<isCall>(S0, r, T, K, historicalSigma, startSigma, maxIterations);
-    std::cout << "The implied volatility is " << impliedVolatility << "." << std::endl;
+//    std::cout << "The implied volatility is " << impliedVolatility << "." << std::endl;
     results[idx++] = impliedVolatility;
   } catch (NoConvergenceError & error) {
-    std::cout << "No convergence! Last sigma was " << error.getLastSigma() << "." << std::endl;
+//    std::cout << "No convergence! Last sigma was " << error.getLastSigma() << "." << std::endl;
     results[idx++] = error.getLastSigma();
   }
 }
 
 template<bool isCall>
-void printResults();
+void runNewtonRaphsonTest(const char * filename);
 
 void main_s4_07() {
-  for (double histSigma: histSigmas) {
-    historicalSigma = histSigma;
-    runTest<CALL>();
-//    runTest<PUT>();
-    for (double sigma: sigmas) {
-      runTest<CALL>(sigma);
-//      runTest<PUT>(sigma);
-    }
-  }
+  runNewtonRaphsonTest<CALL>("data_s4_07_call");
 
-//  for (double result: results) {
-//    std::cout << result << " ";
-//  }
-//  std::cout << std::endl;
-  printResults<CALL>();
-//  std::ofstream out;
-//  out.open("data_s4_07");
-//  // do sth with out
-//  out.close();
+  runNewtonRaphsonTest<PUT>("data_s4_07_put");
 }
 
-#include <cstdio>
+template<bool isCall>
+void printResults(std::ostream & out) {
+  const double defaultSigma = computeDefaultSigma<isCall>(S0, r, T, K, historicalSigma, maxIterations);
+  out << (isCall ? "Call" : "Put") << " values with default sigma " << defaultSigma << ":" << std::endl;
+
+  out << std::setprecision(7) << std::setfill(' ') << std::fixed
+      << "           &" << std::setw(10) << defaultSigma << " &" << std::setw(10) << sigmas[0]
+      << " &" << std::setw(10) << sigmas[1] << " &" << std::setw(10) << sigmas[2] << " &" << std::setw(10) << sigmas[3]
+      << std::endl;
+
+  for (int i = 0; i < 4; ++i) {
+    out << std::setw(10) << histSigmas[i] << " ";
+    for (int j = 0; j < 5; ++j)
+      out << "&" << std::setw(10) << results[i * 5 + j] << " ";
+    out << std::endl;
+  }
+}
 
 template<bool isCall>
-void printResults() {
-  const double defaultSigma = computeDefaultSigma<isCall>(S0, r, T, K, historicalSigma, maxIterations);
-  printf("           %10.7lf %10.7lf %10.7lf %10.7lf %10.7lf \n", defaultSigma, sigmas[0], sigmas[1], sigmas[2], sigmas[3]);
-  for (int i = 0; i < 4; ++i) {
-    printf("%10.7lf ", histSigmas[i]);
-    for (int j = 0; j < 5; ++j) {
-      printf("%10.7lf ", results[i * 4 + j]);
+void runNewtonRaphsonTest(const char * filename) {
+  std::ofstream out;
+  out.open(filename);
+
+  for (double histSigma : histSigmas) {
+    historicalSigma = histSigma;
+    runOneNewtonRaphsonTest<isCall>();
+    for (double sigma : sigmas) {
+      runOneNewtonRaphsonTest<isCall>(sigma);
     }
-    printf("\n");
   }
+
+  printResults<isCall>(out);
+  out.close();
 }
