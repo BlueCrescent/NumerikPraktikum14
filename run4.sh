eb1dfabdc4447392932cdb743b276456632ca6ef
BlueCrescent <BlueCrescent@gmx.de>
2014-06-23 00:18:19 +0200

    Run script for sheet 4.

---
+++ run4.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+mkdir -p Output/Data4
+cd Output/Data4
+
+../../Release/NumerikPraktikum14
+
+
+
+
+
+../../BaSheet04/Task03/sh4_03_price_plot.sh
+mv sh4_task3_price_plot.pdf ../sh4_task3_price_plot.pdf
+
+
