511c71b109db74ba6bae666cbf9830a7f50644e2
boris <boris@boris.boris>
2014-06-26 21:10:08 +0200

    Improved plot of sheet 3 task 17.

--- BaSheet03/Task17/sh3_17_convergencePlot.sh
+++ BaSheet03/Task17/sh3_17_convergencePlot.sh
@@ -5,24 +5,24 @@ set grid
 
 set logscale y
 
-set xlabel "l"
+set xlabel "Number of Nodes"
 set ylabel "relativ Error"
 
 set title "convergence plot for M = 64"
-plot "data_s3_17" using 1:2 with l title "RandWalk - Monte Carlo", \
- "data_s3_17" using 1:3 with l title "RandWalk - Quasi Monte Carlo", \
- "data_s3_17" using 1:4 with l title "BrownBridge - Monte Carlo", \
- "data_s3_17" using 1:5 with l title "BrownBridge - Quasi Monte Carlo"
+plot "data_s3_17" using 1:2 with lp title "RandWalk - Monte Carlo", \
+ "data_s3_17" using 1:3 with lp title "RandWalk - Quasi Monte Carlo", \
+ "data_s3_17" using 1:4 with lp title "BrownBridge - Monte Carlo", \
+ "data_s3_17" using 1:5 with lp title "BrownBridge - Quasi Monte Carlo"
 pause mouse
 
 set term pdf
 
 set output "sh3_task17_convergencePlot.pdf"
 set title "convergence plot for M = 64"
-plot "data_s3_17" using 1:2 with l title "RandWalk - Monte Carlo", \
- "data_s3_17" using 1:3 with l title "RandWalk - Quasi Monte Carlo", \
- "data_s3_17" using 1:4 with l title "BrownBridge - Monte Carlo", \
- "data_s3_17" using 1:5 with l title "BrownBridge - Quasi Monte Carlo"
+plot "data_s3_17" using 1:2 with lp title "RandWalk - Monte Carlo", \
+ "data_s3_17" using 1:3 with lp title "RandWalk - Quasi Monte Carlo", \
+ "data_s3_17" using 1:4 with lp title "BrownBridge - Monte Carlo", \
+ "data_s3_17" using 1:5 with lp title "BrownBridge - Quasi Monte Carlo"
 
 EOT
 #set term pdf color blacktext "Helvetica" 14
